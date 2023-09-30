9f94824cc2ca3700ad174a333b70b80da7f1751b
Roman1510 <romanvinnick@gmail.com>
2023-09-30 18:53:13 +0200

    making a sphere

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-import './style.css'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-   Hi
-  </div>
-`
+const size = {
+  width: innerWidth,
+  height: innerHeight
+}
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, size.width /size.height, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( size.width, size.height );
+document.body.appendChild( renderer.domElement );
+
+
+const geometry = new THREE.SphereGeometry( 5, 50, 50 );
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 15;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
