597be72b077c8634e2b7856a0015f1b5cdb959b1
Roman1510 <romanvinnick@gmail.com>
2023-11-07 12:49:01 +0100

    stars

--- helpers/useStars.js
+++ helpers/useStars.js
@@ -2,15 +2,18 @@ import * as THREE from 'three'
 
 const starGeometry = new THREE.BufferGeometry()
 const starMaterial = new THREE.PointsMaterial({
-  color: 0xffffff
+  size: 0.03,
+  color: 0xffffff,
+  transparent: true,
+  blending: THREE.AdditiveBlending
 })
 
 const starVertices = []
 
 for (let i = 0; i < 1000; i++) {
-  const x = (Math.random() - 0.5) * 2000
-  const y = (Math.random() - 0.5) * 2000
-  const z = (-Math.random()) * 2000
+  const x = (Math.random() - 0.5) * 1500
+  const y = (Math.random() - 0.5) * 1500
+  const z = (-Math.random()) * 1500
   starVertices.push(x, y, z)
 }
 
